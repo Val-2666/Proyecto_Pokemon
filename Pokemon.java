a35536b3d58726fa37b28882948e212a0cd36712
Val-2666 <valeria.ramos.moreno@correounivalle.edu.co>
2025-04-05 21:12:05 -0500

    Correcciones

--- Pokemon.java
+++ Pokemon.java
@@ -1,15 +1,68 @@
+import java.util.ArrayList;
+
 public class Pokemon {
     private String nombre;
     private String tipo;
-    private int hp;
+    private int puntosSalud;
 
-    public Pokemon(String nombre, String tipo, int hp) {
+    // Comentamos esto porque la clase Ataque aún no existe
+    // private ArrayList<Ataque> ataques;
+
+    public Pokemon(String nombre, String tipo, int puntosSalud) {
         this.nombre = nombre;
         this.tipo = tipo;
-        this.hp = hp;
+        this.puntosSalud = puntosSalud;
+        // this.ataques = new ArrayList<>();
     }
 
-    public void mostrarInfo() {
-        System.out.println("Nombre: " + nombre + ", Tipo: " + tipo + ", HP: " + hp);
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getPuntosSalud() {
+        return puntosSalud;
+    }
+
+    public void setPuntosSalud(int puntosSalud) {
+        this.puntosSalud = puntosSalud;
+    }
+
+    // public ArrayList<Ataque> getAtaques() {
+    //     return ataques;
+    // }
+
+    // public void agregarAtaque(Ataque ataque) {
+    //     if (ataques.size() < 4) {
+    //         ataques.add(ataque);
+    //     } else {
+    //         System.out.println("Este Pokémon ya tiene 4 ataques.");
+    //     }
+    // }
+
+    public void recibirDanio(int danio) {
+        puntosSalud -= danio;
+        if (puntosSalud < 0) {
+            puntosSalud = 0;
+        }
+    }
+
+    // public void atacar(Pokemon enemigo, Ataque ataque) {
+    //     int danio = ataque.getPotencia();
+    //     if (tieneVentaja(this.tipo, enemigo.getTipo())) {
+    //         danio += danio * 0.3;
+    //         System.out.println("¡Es súper efectivo!");
+    //     }
+    //     System.out.println(this.nombre + " usa " + ataque.getNombre() + " contra " + enemigo.getNombre());
+    //     enemigo.recibirDanio(danio);
+    // }
+
+    private boolean tieneVentaja(String tipoAtacante, String tipoDefensor) {
+        return (tipoAtacante.equals("Fuego") && tipoDefensor.equals("Planta")) ||
+               (tipoAtacante.equals("Agua") && tipoDefensor.equals("Fuego")) ||
+               (tipoAtacante.equals("Planta") && tipoDefensor.equals("Agua"));
     }
 }
