b2be29b81cfeb2e0ef740d77d249a301efc6914e
Val-2666 <valeria.ramos.moreno@correounivalle.edu.co>
2025-04-06 17:03:16 -0500

    Correcciones sobre ataque

--- Pokemon.java
+++ Pokemon.java
@@ -5,7 +5,6 @@ public class Pokemon {
     private String tipo;
     private int puntosSalud;
 
-    // Comenté esto porque la clase Ataque aún no existe
     // private ArrayList<Ataque> ataques;
 
     public Pokemon(String nombre, String tipo, int puntosSalud) {
@@ -31,18 +30,6 @@ public class Pokemon {
         this.puntosSalud = puntosSalud;
     }
 
-    // public ArrayList<Ataque> getAtaques() {
-    //     return ataques;
-    // }
-
-    // public void agregarAtaque(Ataque ataque) {
-    //     if (ataques.size() < 4) {
-    //         ataques.add(ataque);
-    //     } else {
-    //         System.out.println("Este Pokémon ya tiene 4 ataques.");
-    //     }
-    // }
-
     public void recibirDanio(int danio) {
         puntosSalud -= danio;
         if (puntosSalud < 0) {
@@ -50,19 +37,8 @@ public class Pokemon {
         }
     }
 
-    // public void atacar(Pokemon enemigo, Ataque ataque) {
-    //     int danio = ataque.getPotencia();
-    //     if (tieneVentaja(this.tipo, enemigo.getTipo())) {
-    //         danio += danio * 0.3;
-    //         System.out.println("¡Es súper efectivo!");
-    //     }
-    //     System.out.println(this.nombre + " usa " + ataque.getNombre() + " contra " + enemigo.getNombre());
-    //     enemigo.recibirDanio(danio);
-    // }
-
-    private boolean tieneVentaja(String tipoAtacante, String tipoDefensor) {
-        return (tipoAtacante.equals("Fuego") && tipoDefensor.equals("Planta")) ||
-               (tipoAtacante.equals("Agua") && tipoDefensor.equals("Fuego")) ||
-               (tipoAtacante.equals("Planta") && tipoDefensor.equals("Agua"));
+    // Unico punto de interacción con Ataque
+    public void usarAtaque(Ataque ataque, Pokemon enemigo) {
+        ataque.atacar(this, enemigo); // 'this' es el atacante
     }
 }
